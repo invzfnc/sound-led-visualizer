eb9417b875aff189a861a1f67ede22975ce9be9d
cha <t1110020m@gmail.com>
2026-08-24 15:22:53 +0800

    test microphone

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,31 @@
 #define MAX_BARS 7
 #define PWM_FREQ 5000
 #define PWM_RES 8
+#define MIC_PIN 19
+
+void setup_led();
+void loop_led();
+void loop_mic();
 
 const int ledPins[MAX_BARS] = {21, 47, 38, 39, 40, 41, 42};
 
 void setup() {
   Serial.begin(115200);
   Serial.println("boot ok");
+}
 
+void loop() {
+  loop_mic();
+}
+
+void setup_led() {
   for (int i = 0; i < MAX_BARS; i++) {
     ledcSetup(i, PWM_FREQ, PWM_RES);
     ledcAttachPin(ledPins[i], i);
   }
 }
 
-void loop() {
+void loop_led() {
   for (int b = 0; b <= 255; b++) {
     for (int i = 0; i < MAX_BARS; i++)
       ledcWrite(i, b);
@@ -29,4 +40,11 @@ void loop() {
   }
 
   delay(500);
+}
+
+void loop_mic() {
+  int sample = analogRead(MIC_PIN);
+  if (sample - 180 > 10)
+    Serial.println(sample);
+  delay(2);
 }

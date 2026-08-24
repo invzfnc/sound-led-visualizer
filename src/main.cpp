dfc4fa3195551b3bf0285761c218211f55038a7c
cha <t1110020m@gmail.com>
2026-08-25 02:28:40 +0800

    works with low accuracy and high fluctuation

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,68 @@
 #include <Arduino.h>
 
 #define MAX_BARS 7
-#define PWM_FREQ 5000
-#define PWM_RES 8
 #define MIC_PIN 19
-
-void setup_led();
-void loop_led();
-void loop_mic();
+#define SAMPLE_WINDOW 50
+#define DELAY 50
 
 const int ledPins[MAX_BARS] = {21, 47, 38, 39, 40, 41, 42};
+unsigned long startMillis;
+
+int calculateBars();
+void updateLeds(int);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("boot ok");
+
+  for (int i = 0; i < MAX_BARS; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 }
 
 void loop() {
-  loop_mic();
+  int bars = calculateBars();
+  Serial.print("bars: ");
+  Serial.println(bars);
+  updateLeds(bars);
+  delay(DELAY);
 }
 
-void setup_led() {
+/* translates volume detected to bar/level */
+int calculateBars() {
+  unsigned int signalMin = 1024;
+  unsigned int signalMax = 0;
+  unsigned int signal;
+
+  startMillis = millis();
+
+  while (millis() - startMillis < SAMPLE_WINDOW) {
+    signal = analogRead(MIC_PIN);
+    if (signal > signalMax)
+      signalMax = signal;
+    if (signal < signalMin) 
+      signalMin = signal;
+  }
+
+  // debug
+  Serial.print("signalMin: ");
+  Serial.print(signalMin);
+  Serial.print(" | signalMax: ");
+  Serial.print(signalMax);
+  Serial.print(" | signal: ");
+  Serial.println(signal);
+
+  return map(signal, signalMin, signalMax, 0, MAX_BARS);
+}
+
+/* turn on and turn off LEDs given number of lit bars */
+void updateLeds(int bars) {
   for (int i = 0; i < MAX_BARS; i++) {
-    ledcSetup(i, PWM_FREQ, PWM_RES);
-    ledcAttachPin(ledPins[i], i);
+    if (i <= bars) {
+      digitalWrite(ledPins[i], HIGH);
+    }
+    else {
+      digitalWrite(ledPins[i], LOW);
+    }
   }
-}
-
-void loop_led() {
-  for (int b = 0; b <= 255; b++) {
-    for (int i = 0; i < MAX_BARS; i++)
-      ledcWrite(i, b);
-    delay(4);
-  }
-  for (int b = 255; b >= 0; b--) {
-    for (int i = 0; i < MAX_BARS; i++) 
-      ledcWrite(i, b);
-    delay(4);
-  }
-
-  delay(500);
-}
-
-void loop_mic() {
-  int sample = analogRead(MIC_PIN);
-  if (sample - 180 > 10)
-    Serial.println(sample);
-  delay(2);
 }

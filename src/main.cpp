a97765236a6d64b605b792ebda6b7c673e8c9e26
cha <t1110020m@gmail.com>
2026-08-26 16:20:45 +0800

    refactor: variable renames, spacing, debug toggle

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,33 @@
 
 //#define DEBUG  // comment this to disable debug
 
-#define MAX_BARS 7
+#define MAX_LEVEL 7
 #define MIC_PIN 19
 
 // these three are dependant
 // less samples gives less sensitive and less accurate results, but less delay
 // more samples gives more accurate results, but more delay
 #define SAMPLE_COUNT 256 // tested sweet spot between 256 and 512
-#define LOOP_DELAY 1  // milliseconds
-#define READ_DELAY 50 // microseconds
+#define LOOP_DELAY   1  // milliseconds
+#define READ_DELAY   50 // microseconds
 
 #define DECAY_RATE 0.996f
-#define MIN_RMS 4.0f  // adjust this to debug mode observations
+#define MIN_RMS    4.0f  // adjust this to debug mode observations
 
-const int ledPins[MAX_BARS] = {21, 47, 38, 39, 40, 41, 42};
+const int ledPins[MAX_LEVEL] = {21, 47, 38, 39, 40, 41, 42};  // match gpio
+
 float maxRms = 0;
 int samples[SAMPLE_COUNT];
 
 void updateLeds(int);
 
 void setup() {
-  Serial.begin(115200);
+  #ifdef DEBUG
+    Serial.begin(115200);
+    Serial.println("boot ok");
+  #endif
 
-  for (int i = 0; i < MAX_BARS; i++) {
+  for (int i = 0; i < MAX_LEVEL; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
 }
@@ -67,7 +71,7 @@ void loop() {
   }
 
   float normalized = constrain(rms / maxRms, 0.0, 1.0);
-  int bars = (int)(normalized * MAX_BARS);
+  int level = (int)(normalized * MAX_LEVEL);
   
   #ifdef DEBUG
     Serial.print("dcBias: ");
@@ -78,18 +82,18 @@ void loop() {
     Serial.print(rms);
     Serial.print(" | maxRms: ");
     Serial.print(maxRms);
-    Serial.print(" | bars: ");
-    Serial.println(bars);
+    Serial.print(" | level: ");
+    Serial.println(level);
   #endif
 
-  updateLeds(bars);
+  updateLeds(level);
   delay(LOOP_DELAY);
 }
 
-/* turn on and turn off LEDs given number of lit bars */
-void updateLeds(int bars) {
-  for (int i = 0; i < MAX_BARS; i++) {
-    if (i < bars) {
+/* turn on and turn off LEDs given level number */
+void updateLeds(int level) {
+  for (int i = 0; i < MAX_LEVEL; i++) {
+    if (i < level) {
       digitalWrite(ledPins[i], HIGH);
     }
     else {
